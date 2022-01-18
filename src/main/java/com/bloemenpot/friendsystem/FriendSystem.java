41d9c87b7764f91310b438c44aee099db3d0d054
Bloemenpot <mjssmith03@gmail.com>
2022-01-18 01:12:16 +0100

    Created Project

---
+++ src/main/java/com/bloemenpot/friendsystem/FriendSystem.java
@@ -0,0 +1,11 @@
+package com.bloemenpot.friendsystem;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class FriendSystem extends Plugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("FriendSystem Enabled!");
+    }
+}
